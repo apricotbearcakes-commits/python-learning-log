47234d8fb5e46493bdee6fced1e7039ea9b81ecb
apricotbearcakes-commits <apricotbearcakes@gmail.com>
2026-04-05 19:34:25 -0700

    Update day09_while_if.py

--- day09_while_if.py
+++ day09_while_if.py
@@ -1,1 +1,28 @@
+count = 1
 
+while count <= 5:
+    print(count)
+    count = count + 1
+  
+number = int(input("数字を入力してください："))
+count = 1
+
+while count <= number:
+    if count % 2 == 0:
+        print(count)
+    count = count + 1
+
+print(4 % 2)
+print(5 % 2)
+print(6 % 2)
+print(7 % 2)
+
+number = int(input("数字を入力してください："))
+count = 1
+total = 0
+
+while count <= number:
+    total = total + count
+    count = count + 1
+
+print(f"合計：{total}")
