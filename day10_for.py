aa60b782b98d5fe0580bccbcf93c2a34676f9ae2
apricotbearcakes-commits <apricotbearcakes@gmail.com>
2026-04-05 20:44:23 -0700

    Update day10_for.py

--- day10_for.py
+++ day10_for.py
@@ -1,2 +1,31 @@
 for count in range(1, 6):
     print(count)
+
+for count in range(3, 8):
+    print(count)
+
+for count in range(0, 10, 2):
+    print(count)
+
+total = 0
+for count in range(1, 11):
+    total = total + count
+
+print(f"合計：{total}")
+
+number = int(input("数字を入力してください："))
+total = 0
+for count in range(1, number + 1):
+    total = total + count
+
+print(f"合計：{total}")
+
+name = input("名前を入力してください：")
+
+for i in range(5):
+    print(name)
+
+name = input("名前を入力してください：")
+
+for i in range(1, 6):
+    print(f"{i}回目：{name}")
