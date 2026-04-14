1bb9fbf0eb1ba2863ebe63b8fb309ebf2d83615b
apricotbearcakes-commits <apricotbearcakes@gmail.com>
2026-04-13 17:33:07 -0700

    Update day12_key_for.py

--- day12_key_for.py
+++ day12_key_for.py
@@ -1,1 +1,25 @@
+person = {"name": "Masako", "job": "Data Annotator", "city": "Sunnyvale"}
+
+print(person["name"])
+print(person["job"])
+print(person["city"])
+
+person = {"name": "Masako", "job": "Data Annotator"}
+
+person["city"] = "Sunnyvale"
+print(person)
+
+person["job"] = "Python Developer"
+print(person)
+
+person = {"name": "Masako", "job": "Data Annotator", "city": "Sunnyvale"}
+
+for key in person:
+    print(key, ":", person[key])
+
+person = {"name": "Masako", "job": "Data Annotator", "city": "Sunnyvale", "hobby": "Design", "goal": "Apple"}
+
+for key in person:
+    print(key, ":", person[key])
+
 
