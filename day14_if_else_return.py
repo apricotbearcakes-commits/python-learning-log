13a5dba8c250414321ddaae34988d20c16967aef
apricotbearcakes-commits <apricotbearcakes@gmail.com>
2026-04-16 21:55:41 -0700

    Update day14_if_else_return.py

--- day14_if_else_return.py
+++ day14_if_else_return.py
@@ -1,1 +1,15 @@
+def add(a, b):
+    return a + b
 
+result = add(10, 5)
+print(result)
+
+
+def check_score(name, score):
+    if score >= 80:
+        return (f"{name}さん！合格です！")
+    else:
+        return (f"{name}さん！不合格です！")
+
+print(check_score("Masako", 85))
+print(check_score("Hana", 60))
